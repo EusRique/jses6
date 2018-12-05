c6f6f56cb15728034c67af5fa6ae88000342e5a3
Henrique <hiquesc_@hotmail.com>
2018-12-04 22:14:29 -0200

    Configurações de ambiente

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-alert('Opa');
+const minhaPromise = () => new Promise((resolve, reject) =>{
+    setTimeout(() => { resolve('OK') }, 2000);
+});
+
+async function executaPromise() {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());  
+}
+
+executaPromise();
